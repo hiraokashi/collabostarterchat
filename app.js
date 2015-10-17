fec320e8c95c293a87533902a6066c9852b75e5e
Takashi Hiraoka <hiraokashi@gmail.com>
2015-10-17 23:07:08 +0900

    cookieに対応

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ RingBuffer.prototype =
 
 var ejs = require('ejs');
 var fs = require("fs");
+var cookie   = require('cookie');
 
 //再接続のために100件のデータは保持しておく
 var msgBuffer = new RingBuffer(100);
@@ -58,16 +59,27 @@ var indexEJS = fs.readFileSync('./index.ejs', 'utf8');
 var userHash = {};
 
 var server = require("http").createServer(function(req, res) {
-    var user = "";
-    var ip = ipaddress(req);
 
-    if (ip in userHash) {
-      user = userHash[ip];
-      console.log("[INFO] IP ADDRESS found %s", ip);
+    var user = "";
+    //var cookie_data = "";
+    if (req.headers.cookie === undefined) {
+      var d = new Date().toDateString();
+      var serialized_cookie1 = cookie.serialize('uniqueID', Math.random().toString(36).slice(-8), {
+          maxAge : 12000 //有効期間を3600 * 3 秒に設定
+      });
+      res.setHeader('Set-Cookie', serialized_cookie1);
+      //Object.keys( serialized_cookie1 ); //=> []
+      console.log("おはつにおめにかかります");
+      userHash[serialized_cookie1.uniqueID] = "";
+      //cookie_data = serialized_cookie1.uniqueID;
     } else {
-      console.log("[INFO] IP ADDRESS not found(新規接続) %s", ip);
+      var parsed_cookie = cookie.parse(req.headers.cookie);
+      console.log("[INFO] parsed_cookie %s", Object.keys(parsed_cookie));
+      user = userHash[parsed_cookie.uniqueID];
+      console.log(user);
+      //cookie_data = parsed_cookie.uniqueID;
     }
-    console.log("[INFO] previousUsrs.length= ", usrBuffer.count);
+
     var hokuto = ejs.render(indexEJS, {
         user: user,
         previousMsgs: msgBuffer,
@@ -98,54 +110,37 @@ io.sockets.on("connection", function (socket) {
 
   // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
   socket.on("connected", function (name) {
-    userHash[socket.handshake.address] = name;
-    console.log("[INFO] %s, %s connected", name, socket.handshake.address);
-    io.sockets.emit("publish", {value: "入室しました", user: name, type: "start"});
+    cookie_data = socket.handshake.headers.cookie.replace(/.+uniqueID=([a-zA-Z0-9]{8})$/gi, "$1");
+    userHash[cookie_data] = name;
+    console.log("[INFO] %s, %s connected", userHash[cookie_data] , cookie_data);
+    io.sockets.emit("publish", {value: "入室しました", user: userHash[cookie_data] , type: "start"});
   });
 
   // 再接続カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
   socket.on("reconnected", function (name) {
-    console.log("[INFO] %s, %s 再接続", userHash[socket.handshake.address], socket.handshake.address);
-    io.sockets.emit("publish", {value: "再入室しました", user:userHash[socket.handshake.address] , type: "restart"});
+    cookie_data = socket.handshake.headers.cookie.replace(/.+uniqueID=([a-zA-Z0-9]{8})$/gi, "$1");
+    console.log("[INFO] %s, %s 再接続", userHash[cookie_data], cookie_data);
+    io.sockets.emit("publish", {value: "再入室しました", user:userHash[cookie_data] , type: "restart"});
   });
 
   // メッセージ送信カスタムイベント
   socket.on("publish", function (data) {
     //console.log(data);
+    cookie_data = socket.handshake.headers.cookie.replace(/.+uniqueID=([a-zA-Z0-9]{8})$/gi, "$1");
     msgBuffer.add(data);
-    usrBuffer.add(userHash[socket.handshake.address]);
-    console.log("[INFO] %s, %s publish", userHash[socket.handshake.address], socket.handshake.address);
-    io.sockets.emit("publish", {value:data.value.replace(/(https?:\/\/[\x21-\x7e]+)/gi, "<a href='$1'>$1</a>"), user:userHash[socket.handshake.address], type: "normal"});
+    usrBuffer.add(userHash[cookie_data]);
+    console.log("[INFO] %s, %s publish", userHash[cookie_data], cookie_data);
+    io.sockets.emit("publish", {value:data.value.replace(/(https?:\/\/[\x21-\x7e]+)/gi, "<a href='$1'>$1</a>"), user:userHash[cookie_data], type: "normal"});
   });
 
   // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
   socket.on("disconnect", function () {
-    if (userHash[socket.handshake.address]) {
-      console.log("[INFO] %s, %s 退出", userHash[socket.handshake.address], socket.handshake.address);
-      var msg = userHash[socket.id] + "が退出しました";
-      io.sockets.emit("publish", {value: "退室しました", user: userHash[socket.handshake.address], type: "end"});
-      delete userHash[socket.id];
+    cookie_data = socket.handshake.headers.cookie.replace(/.+uniqueID=([a-zA-Z0-9]{8})$/gi, "$1");
+    if (userHash[cookie_data]) {
+      console.log("[INFO] %s, %s 退出", userHash[cookie_data], cookie_data);
+      var msg = userHash[cookie_data] + "が退出しました";
+      io.sockets.emit("publish", {value: "退室しました", user: userHash[cookie_data], type: "end"});
+      //delete userHash[cookie_data];
     }
   });
 });
-
-
-function ipaddress (request) {
-
-//  console.log("request.headers['x-forwarded-for'] = " + request.headers['x-forwarded-for']);
-  //console.log("request.connection = " + request.connection);
-  //console.log("request.connection.remoteAddress = " + request.connection.remoteAddress);
-//  console.log("request.connection.socket = " + request.connection.socket);
-//  console.log("request.connection.socket.remoteAddress = " + request.connection.socket.remoteAddress);
-//  return request.headers['x-forwarded-for']
-//      ? request.headers['x-forwarded-for']
-//      : (request.connection && request.connection.remoteAddress)
-//      ? request.connection.remoteAddress
-//      : (request.connection.socket && request.connection.socket.remoteAddress)
-//      ? request.connection.socket.remoteAddress
-//      : (request.socket && request.socket.remoteAddress)
-//      ? request.socket.remoteAddress
-//      : '0.0.0.0';
-return request.connection.remoteAddress;
-
-}
