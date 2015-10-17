0ae1244a7d2266805be18d4de991f52f68305733
Takashi Hiraoka <hiraokashi@gmail.com>
2015-10-17 17:44:07 +0900

    IP取得方法修正

--- app.js
+++ app.js
@@ -74,19 +74,20 @@ io.sockets.on("connection", function (socket) {
 
 
 function ipaddress (request) {
-  console.log("request.headers['x-forwarded-for'] = " + request.headers['x-forwarded-for']);
-  console.log("request.connection = " + request.connection);
-  console.log("request.connection.remoteAddress = " + request.connection.remoteAddress);
-  console.log("request.connection.socket = " + request.connection.socket);
-  console.log("request.connection.socket.remoteAddress = " + request.connection.socket.remoteAddress);
-  return request.headers['x-forwarded-for']
-      ? request.headers['x-forwarded-for']
-      : (request.connection && request.connection.remoteAddress)
-      ? request.connection.remoteAddress
-      : (request.connection.socket && request.connection.socket.remoteAddress)
-      ? request.connection.socket.remoteAddress
-      : (request.socket && request.socket.remoteAddress)
-      ? request.socket.remoteAddress
-      : '0.0.0.0';
+    return request.connection.remoteAddress;
+//  console.log("request.headers['x-forwarded-for'] = " + request.headers['x-forwarded-for']);
+//  console.log("request.connection = " + request.connection);
+//  console.log("request.connection.remoteAddress = " + request.connection.remoteAddress);
+//  console.log("request.connection.socket = " + request.connection.socket);
+//  console.log("request.connection.socket.remoteAddress = " + request.connection.socket.remoteAddress);
+//  return request.headers['x-forwarded-for']
+//      ? request.headers['x-forwarded-for']
+//      : (request.connection && request.connection.remoteAddress)
+//      ? request.connection.remoteAddress
+//      : (request.connection.socket && request.connection.socket.remoteAddress)
+//      ? request.connection.socket.remoteAddress
+//      : (request.socket && request.socket.remoteAddress)
+//      ? request.socket.remoteAddress
+//      : '0.0.0.0';
 
 }
